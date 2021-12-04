b8eb628fc7a49940dfd537bc07d2bee8ee3fb277
W4l14c3 <90666417+W4l14c3@users.noreply.github.com>
2021-12-04 14:58:19 -0300

    First Commit

---
+++ VendasBNS/src/persistencia/CadProd.java
@@ -0,0 +1,84 @@
+
+package persistencia;
+
+/**
+ *
+ * @author Wallace
+ */
+public class CadProd {
+    private String codigo;
+    private String nome;
+    private String CodigodeBarras;
+    private String categoria;
+    private String img;
+    private String data;
+    private String hora;
+    private String valor;
+    public String getCodigodeBarras() {
+        return CodigodeBarras;
+    }
+
+    public void setCodigodeBarras(String CodigodeBarras) {
+        this.CodigodeBarras = CodigodeBarras;
+    }
+    
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+}
